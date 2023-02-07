f88070fc06f8b7cf60971c8e7f2f566378fc9efc
enjoy301 <eunjong147@pusan.ac.kr>
2023-02-07 15:57:35 +0900

    134p

--- Ch3/file-struct/src/main.rs
+++ Ch3/file-struct/src/main.rs
@@ -1,20 +1,38 @@
 #![allow(unused_variables)]
 
+use std::fmt;
+use std::fmt::{Display};
+
 #[derive(Debug, PartialEq)]
-enum FileState {
+pub enum FileState {
     Open,
     Close,
 }
 
 #[derive(Debug)]
-struct File {
-    name: String,
+pub struct File {
+    pub name: String,
     data: Vec<u8>,
-    state: FileState,
+    pub state: FileState,
+}
+
+impl Display for FileState {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match *self {
+            FileState::Open => write!(f, "OPEN"),
+            FileState::Close => write!(f, "CLOSED"),
+        }
+    }
+}
+
+impl Display for File {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "<{} ({})>", self.name, self.state)
+    }
 }
 
 impl File {
-    fn new(name: &str, data: &Vec<u8>) -> File {
+    pub fn new(name: &str, data: &Vec<u8>) -> File {
         File {
             name: String::from(name),
             data: data.clone(),
@@ -22,7 +40,7 @@ impl File {
         }
     }
 
-    fn read(self: &File, save_to: &mut Vec<u8>) -> Result<usize, String> {
+    pub fn read(self: &File, save_to: &mut Vec<u8>) -> Result<usize, String> {
         if self.state != FileState::Open {
             return Err(String::from("error ㅡㅡ"));
         }
@@ -35,19 +53,21 @@ impl File {
 
         Ok(read_length)
     }
+
+    pub fn open(self: &mut File) -> Result<String, String> {
+        self.state = FileState::Open;
+
+        Ok(String::from("good"))
+    }
+
+    fn close(self: &mut File) -> Result<String, String> {
+        self.state = FileState::Close;
+
+        Ok(String::from("good"))
+    }
 }
 
-fn open(mut f: File) -> Result<File, String> {
-    f.state = FileState::Open;
 
-    Ok(f)
-}
-
-fn close(mut f: File) -> Result<File, String> {
-    f.state = FileState::Close;
-
-    Ok(f)
-}
 
 fn main() {
     let f2_data: Vec<u8> = vec![114, 117, 115, 116, 33];
@@ -55,13 +75,14 @@ fn main() {
 
     let mut buffer: Vec<u8> = vec![];
 
-    f2 = open( f2).unwrap();
+    f2.open().unwrap();
     let f2_length = f2.read(&mut buffer).unwrap();
-    f2 = close(f2).unwrap();
+    f2.close().unwrap();
 
     let text = String::from_utf8_lossy(&buffer);
 
     println!("{:?}", f2);
+    println!("{}", f2);
     println!("{} is {} bytes", &f2.name, f2_length);
     println!("{}", text);
 }
